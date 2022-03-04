3e6a18c1f2675960fa2d8f94283415c2d827cd3a
quangnc <chiquang.dev@gmail.com>
2022-03-04 22:15:10 +0700

    encode decode logon

--- lib/elixir_crawly_chess/protocol/ws_msg.ex
+++ lib/elixir_crawly_chess/protocol/ws_msg.ex
@@ -2,8 +2,14 @@ defmodule ElixirCrawlyChess.Protocol.WSMsg do
   alias ElixirCrawlyChess.Protocol.SearchMsg, as: SearchMsg
 
   def encode(msg) do
-    header_encode = <<msg.header.type::size(16), msg.header.n_val::size(32), msg.header.is_sender::size(32), msg.header.user_type::size(16),
-      msg.header.id_receiver::size(32), msg.header.msg_id::size(32)>>
+    header_encode = <<
+      msg.header.type :: size(16),
+      msg.header.n_val :: size(32),
+      msg.header.is_sender :: size(32),
+      msg.header.user_type :: size(16),
+      msg.header.id_receiver :: size(32),
+      msg.header.msg_id :: size(32)
+    >>
 
     body_encode = case msg.header.type do
       7100 -> SearchMsg.encode(msg.body)
@@ -13,23 +19,49 @@ defmodule ElixirCrawlyChess.Protocol.WSMsg do
     body_encode_size = byte_size(body_encode)
     check_sum_body = check_sum(body_encode)
 
-    header_encode <> <<body_encode_size::little-size(32)>> <> body_encode <> <<check_sum_body::size(16)>>
-
+    header_encode <> <<body_encode_size :: little - size(32)>> <> body_encode <> <<check_sum_body :: size(16)>>
   end
+
+  def encode_no_msg_id(msg) do
+    header_encode = <<
+      msg.header.type :: size(16),
+      msg.header.n_val :: size(32),
+      msg.header.is_sender :: size(32),
+      msg.header.user_type :: size(16),
+      msg.header.id_receiver :: size(32)
+    >>
+
+    body_encode = case msg.header.type do
+      7002 -> LogonData.encode(msg.body)
+      _ -> decode_logon()
+    end
+
+    body_encode_size = byte_size(body_encode)
+    header_encode <> body_encode
+  end
+
 
   def decode_logon() do
 
   end
 
   def decode(bin) do
-    <<type::size(16), n_val::size(32), is_sender::size(32), user_type::size(16),
-      id_receiver::size(32), msg_id::size(32), n_size::little-size(32), body::binary-size(n_size),
-      rest::binary>> = bin
+    <<
+      type :: size(16),
+      n_val :: size(32),
+      is_sender :: size(32),
+      user_type :: size(16),
+      id_receiver :: size(32),
+      msg_id :: size(32),
+      n_size :: little - size(32),
+      body :: binary - size(n_size),
+      rest :: binary
+    >> = bin
 
-      body_decoded = case type do
-        7100 -> SearchMsg.decode(body)
-        _ -> decode_logon()
-      end
+    body_decoded = case type do
+      7100 -> SearchMsg.decode(body)
+      _ -> decode_logon()
+    end
 
     %{
       header: %{
@@ -45,16 +77,49 @@ defmodule ElixirCrawlyChess.Protocol.WSMsg do
     }
   end
 
+  def decode_no_msg_id(bin) do
+    IO.inspect(bin)
+
+    <<
+      type :: size(16),
+      n_val :: size(32),
+      is_sender :: size(32),
+      user_type :: size(16),
+      id_receiver :: size(32),
+      n_size :: little - size(32),
+      body :: binary - size(n_size),
+      rest :: binary
+    >> = bin
+
+    body_decoded = case type do
+      7002 -> LogonData.decode(body)
+      _ -> decode_logon()
+    end
+
+    %{
+      header: %{
+        type: type,
+        n_val: n_val,
+        is_sender: is_sender,
+        user_type: user_type,
+        id_receiver: id_receiver,
+      },
+      body: body_decoded,
+      checkSum: rest
+    }
+  end
+
 
   def check_sum(bin) do
-        do_check_sum(bin, 0, 0)
-    end
+    do_check_sum(bin, 0, 0)
+  end
 
-    defp do_check_sum(<<>>, _, sum) do rem(sum, 0x7fff) end
-    defp do_check_sum(<<b, rest::binary>>, i, sum) do
-        sum = (sum + b + i)
-        do_check_sum(rest, i+1, sum)
-    end
+  defp do_check_sum(<<>>, _, sum) do
+    rem(sum, 0x7fff) end
+  defp do_check_sum(<<b, rest :: binary>>, i, sum) do
+    sum = (sum + b + i)
+    do_check_sum(rest, i + 1, sum)
+  end
 
 
 end
--- test/elixir_crawly_chess_test.exs
+++ test/elixir_crawly_chess_test.exs
@@ -1,17 +1,29 @@
 defmodule ElixirCrawlyChessTest do
-    alias ElixirCrawlyChess.Protocol.WSMsg, as: WSMsg
+  alias ElixirCrawlyChess.Protocol.WSMsg, as: WSMsg
 
   use ExUnit.Case
   doctest ElixirCrawlyChess
 
   test "greets the world" do
-     bin =
+    bin =
       "G7wAAAAAAA8tBgAAAAAAAQAAAAc6AAAANgAAAAEJAAAAIE1hcnRpbmV6AAAAAAAAAAAAAAAAAAAAALgLAAAAAAAAAAAAAAAA//9wAQAAAAAAAA1R"
-      bin_decode = bin |> Base.decode64!()
-      ws_msg_decode = WSMsg.decode(bin_decode)
-      ws_msg_encode = WSMsg.encode(ws_msg_decode)
-      IO.inspect(bin_decode, limit: :infinity)
-      IO.inspect(ws_msg_encode, limit: :infinity)
-      assert bin_decode == ws_msg_encode
+    bin_decode = bin
+                 |> Base.decode64!()
+    ws_msg_decode = WSMsg.decode(bin_decode, true)
+    ws_msg_encode = WSMsg.encode(ws_msg_decode)
+    IO.inspect(bin_decode, limit: :infinity)
+    IO.inspect(ws_msg_encode, limit: :infinity)
+    assert bin_decode == ws_msg_encode
+  end
+
+  test "logon" do
+    bin = "G1oAAAAAAAAAAAAAAAAAAdwAAAACAAAATQQFAAUAAABTAAAAAAAAAAAAAAAFAAAAR3Vlc3QEAAAAUGFzc+tM6ED1wch6cOxacyxhieoCAAAAdmkIAAAATWFjSW50ZWzkBAAAAAAAAAAAAAAAAAAAAAAAAIAAAABxAAAANS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS85OC4wLjQ3NTguMTA5IFNhZmFyaS81MzcuMzYAAAAA"
+    bin_decode = bin
+                 |> Base.decode64!()
+    ws_msg_decode = WSMsg.decode_no_msg_id(bin_decode)
+    ws_msg_encode = WSMsg.encode_no_msg_id(ws_msg_decode)
+    IO.inspect(ws_msg_decode, limit: :infinity)
+    IO.inspect(ws_msg_encode, limit: :infinity)
+    assert bin_decode == ws_msg_encode
   end
 end
