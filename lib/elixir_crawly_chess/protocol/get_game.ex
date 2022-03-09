054433b7b0122073682ec1aaddc9eb527084b357
quangnc <chiquang.dev@gmail.com>
2022-03-09 21:20:26 +0700

    change name function

--- lib/elixir_crawly_chess/protocol/get_game.ex
+++ lib/elixir_crawly_chess/protocol/get_game.ex
@@ -4,13 +4,13 @@ defmodule GetGame do
         game_no: game_no
       }) do
     bin_body = BinUtils.write_int(arr_ids)
-    bin_game = bin_game(game_no)
+    bin_game = list_to_bin(game_no)
     bin_body <> bin_game
   end
 
   def decode(bin) do
     {arr_ids, rest} = BinUtils.read_int(bin)
-    game_no = list_game_no(rest)
+    game_no = bin_to_list(rest)
 
     %{
       arr_ids: arr_ids,
@@ -18,29 +18,29 @@ defmodule GetGame do
     }
   end
 
-  def bin_game(arr) do
-    do_bin_game(arr, <<>>)
+  def list_to_bin(arr) do
+    do_list_to_bin(arr, <<>>)
   end
 
-  defp do_bin_game([], bin) do
+  defp do_list_to_bin([], bin) do
     bin
   end
 
-  defp do_bin_game([head | tail], bin) do
+  defp do_list_to_bin([head | tail], bin) do
     bin = bin <> BinUtils.write_int(head)
-    do_bin_game(tail, bin)
+    do_list_to_bin(tail, bin)
   end
 
-  def list_game_no(bin) do
-    do_game_no(bin, [])
+  def bin_to_list(bin) do
+    do_bin_to_list(bin, [])
   end
 
-  defp do_game_no(<<>>, list) do
+  defp do_bin_to_list(<<>>, list) do
     list
   end
 
-  defp do_game_no(<<n_game_no::little-size(32), rest::binary>>, list) do
+  defp do_bin_to_list(<<n_game_no::little-size(32), rest::binary>>, list) do
     list = list ++ [n_game_no]
-    do_game_no(rest,  list)
+    do_bin_to_list(rest,  list)
   end
 end
